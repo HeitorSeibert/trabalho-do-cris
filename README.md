193c2657bc3102e2c496c53fad024bf4cc6ba7f4
Heitor Seibert <heitorseibertdossantos@gmail.com>
2023-04-13 18:50:02 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# trabalho-do-cris" 
+"# trabalho-do-curso" 
---
+++ trabalho questoes/questão 2/trabalho.js
@@ -0,0 +1,5 @@
+//<h1>Tabuada</h1>
+     //  <label for="numero">Digite um número:</label>
+    //<input type="text" id="numero" name="numero">
+    // <button onclick="exibirTabuada()">Exibir Tabuada</button>
+       //<pre id="resultado"></
